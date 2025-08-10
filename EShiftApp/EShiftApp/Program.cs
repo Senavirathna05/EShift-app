f8429fd30c5acef3569d19d0450c16b9ccb8a0cd
Senavirathna05 <sahansenavirathna05@gmail.com>
2025-08-10 23:47:02 +0530

    Add files via upload

---
+++ EShiftApp/EShiftApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using EShiftApp.Admin_Forms;
+using EShiftApp.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EShiftApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AdminDashBoard());
+        }
+    }
+}
